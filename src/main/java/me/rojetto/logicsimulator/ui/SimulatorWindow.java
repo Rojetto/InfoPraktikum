babd97e003a8a5cb0dff7ef0ce4e783bf8c21567
Robert Heedt <robert.heedt@gmail.com>
2015-07-12 11:35:23 +0200

    GUI Verbesserungen Sauberere Fehlermeldungen Minimale Layoutänderungen

--- src/main/java/me/rojetto/logicsimulator/ui/SimulatorWindow.java
+++ src/main/java/me/rojetto/logicsimulator/ui/SimulatorWindow.java
@@ -1,6 +1,7 @@
 package me.rojetto.logicsimulator.ui;
 
 import me.rojetto.logicsimulator.LogicSimulator;
+import me.rojetto.logicsimulator.LogicSimulatorException;
 import me.rojetto.logicsimulator.core.SimulationResult;
 import me.rojetto.logicsimulator.files.DiagramCreator;
 import me.rojetto.logicsimulator.files.ErgCreator;
@@ -12,7 +13,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.PrintStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 import static javax.swing.SpringLayout.*;
 
@@ -27,12 +29,15 @@ public class SimulatorWindow extends JFrame implements ActionListener {
     private JButton helpButton;
     private JLabel imageLabel;
 
+    private final PrintWriter guiOut;
+
     public SimulatorWindow() {
         super("Logic Simulator");
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (Exception e) {
+            e.printStackTrace();
         }
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -40,16 +45,13 @@ public class SimulatorWindow extends JFrame implements ActionListener {
         textArea = new JTextArea();
         textArea.setEditable(false);
         textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-        PrintStream p = new PrintStream(new TextAreaOutputStream(textArea));
-        System.setOut(p); // TODO: Neeeeeee
-        System.setErr(p);
+        guiOut = new PrintWriter(new TextAreaOutputStream(textArea), true);
         JScrollPane scrollPane = new JScrollPane(textArea);
         scrollPane.setPreferredSize(new Dimension(400, 300));
         add(scrollPane, BorderLayout.CENTER);
 
         SpringLayout inputLayout = new SpringLayout();
         inputPanel = new JPanel(inputLayout);
-        inputPanel.setPreferredSize(new Dimension(400, 100));
         add(inputPanel, BorderLayout.NORTH);
 
         circuitField = new JTextField();
@@ -74,24 +76,25 @@ public class SimulatorWindow extends JFrame implements ActionListener {
         helpButton.addActionListener(this);
         inputPanel.add(helpButton);
 
-        inputLayout.putConstraint(NORTH, circuitField, 5, NORTH, inputPanel);
-        inputLayout.putConstraint(WEST, circuitField, 5, WEST, inputPanel);
         inputLayout.putConstraint(NORTH, circuitButton, 5, NORTH, inputPanel);
         inputLayout.putConstraint(EAST, circuitButton, -5, EAST, inputPanel);
+        inputLayout.putConstraint(WEST, circuitField, 5, WEST, inputPanel);
         inputLayout.putConstraint(EAST, circuitField, -5, WEST, circuitButton);
+        inputLayout.putConstraint(VERTICAL_CENTER, circuitField, 0, VERTICAL_CENTER, circuitButton);
 
         inputLayout.putConstraint(NORTH, eventButton, 5, SOUTH, circuitButton);
         inputLayout.putConstraint(EAST, eventButton, -5, EAST, inputPanel);
         inputLayout.putConstraint(WEST, eventButton, 0, WEST, circuitButton);
-        inputLayout.putConstraint(NORTH, eventField, 0, NORTH, eventButton);
         inputLayout.putConstraint(WEST, eventField, 5, WEST, inputPanel);
         inputLayout.putConstraint(EAST, eventField, -5, WEST, eventButton);
+        inputLayout.putConstraint(VERTICAL_CENTER, eventField, 0, VERTICAL_CENTER, eventButton);
 
         inputLayout.putConstraint(NORTH, simulateButton, 5, SOUTH, eventButton);
         inputLayout.putConstraint(EAST, simulateButton, -5, EAST, inputPanel);
 
         inputLayout.putConstraint(NORTH, helpButton, 0, NORTH, simulateButton);
         inputLayout.putConstraint(EAST, helpButton, -5, WEST, simulateButton);
+        inputLayout.putConstraint(SOUTH, inputPanel, 5, SOUTH, simulateButton);
 
         imageLabel = new JLabel();
         add(new JScrollPane(imageLabel), BorderLayout.SOUTH);
@@ -153,15 +156,14 @@ public class SimulatorWindow extends JFrame implements ActionListener {
                 LogicSimulator simulator = new LogicSimulator(getCircuitFile(), getEventFile());
                 SimulationResult result = simulator.simulate();
 
-                System.out.println(ErgCreator.create(result));
+                guiOut.println(ErgCreator.create(result));
 
                 BufferedImage graph = DiagramCreator.create(result);
                 imageLabel.setIcon(new ImageIcon(graph));
 
                 pack();
-            } catch (Exception e1) {
-                System.out.println("Bei der Simulation ist ein Fehler aufgetreten."); //  TODO: Mmmh, nee
-                System.out.println(e1.getClass().getName() + ": " + e1.getMessage());
+            } catch (LogicSimulatorException | IOException e1) {
+                guiOut.println(e1);
             }
         } else if (e.getSource() == helpButton) {
             JOptionPane.showMessageDialog(this, "HILFE HILFE, WAS MUSS ICH TUN! WAS MACHEN DIE PARAMETER?",
